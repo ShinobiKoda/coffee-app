0fc32843eb4ddc029c5fee0a5c69675c509e7918
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-17 17:00:36 +0100

    feat: update app icon and add CoffeeDetails component

---
+++ app/dashboard/coffee/[id].tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CoffeeDetails = () => {
+  return (
+    <View>
+      <Text>CoffeeDetails</Text>
+    </View>
+  )
+}
+
+export default CoffeeDetails
+
+const styles = StyleSheet.create({})
