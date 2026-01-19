7d88942454962913f54b775e9d119bc8d81a37fa
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-19 15:31:35 +0100

    feat: enhance order component layout and styling; add coffee details section

--- app/order.tsx
+++ app/order.tsx
@@ -5,7 +5,7 @@ import { AnimatedPressable } from "@/components/animations/Reanimated";
 import { Colors } from "@/constants/colors";
 import { fonts } from "@/constants/fonts";
 import { useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const order = () => {
   const transports = ["Deliver", "Pick Up"];
@@ -40,12 +40,14 @@ const order = () => {
           ))}
         </View>
 
-        <Spacer height={28}/>
+        <Spacer height={28} />
         <View style={styles.delivery_container}>
           <Text style={styles.delivery_title}>Delivery Address</Text>
           <View style={{}}>
             <Text style={styles.city}>JL.Kpg Sutoyo</Text>
-            <Text style={styles.address}>Kpg. Sutoyo No. 620, Bilzen, Tanjungbailai.</Text>
+            <Text style={styles.address}>
+              Kpg. Sutoyo No. 620, Bilzen, Tanjungbailai.
+            </Text>
           </View>
           <View style={styles.action_btns_container}>
             <View style={styles.action_btn}>
@@ -57,12 +59,34 @@ const order = () => {
           </View>
         </View>
 
-        <View style={{height: 1, marginVertical: 16, marginHorizontal: 4, backgroundColor: Colors.grey_line}}></View>
+        <View
+          style={{
+            height: 1,
+            marginVertical: 16,
+            marginHorizontal: 4,
+            backgroundColor: Colors.grey_line,
+          }}
+        ></View>
 
-        <View>
-          <View>
-            <View>
-              <Image />
+        <View style={styles.coffees_container}>
+          <View style={styles.coffee_detail_container}>
+            <View style={styles.coffee_detail_description}>
+              <View style={styles.coffee_img_container}>
+                <Image style={styles.coffee_image} alt="Coffee Mini Image" resizeMode="cover"/>
+              </View>
+              <View>
+                <Text style={styles.coffee_name}>Caffe Mocha</Text>
+                <Text style={styles.coffee_tags}>Deep Foam</Text>
+              </View>
+            </View>
+            <View style={styles.cart_action_btns}>
+              <View style={styles.modify_coffee_count}>
+                <Text>-</Text>
+              </View>
+              <Text style={styles.coffee_count}>1</Text>
+              <View style={styles.modify_coffee_count}>
+                <Text>+</Text>
+              </View>
             </View>
           </View>
         </View>
@@ -112,46 +136,105 @@ const styles = StyleSheet.create({
     fontFamily: fonts.semibold,
     color: "white",
   },
-  delivery_container:{
+  delivery_container: {
     display: "flex",
     flexDirection: "column",
-    gap: 16
+    gap: 16,
   },
-  delivery_title:{
+  delivery_title: {
     fontFamily: fonts.semibold,
     fontSize: 16,
-    color: Colors.grey_normal
+    color: Colors.grey_normal,
   },
-  city:{
+  city: {
     fontFamily: fonts.semibold,
     fontSize: 14,
     color: Colors.grey_normal,
-
   },
-  address:{
+  address: {
     fontFamily: fonts.regular,
     fontSize: 12,
-    color: Colors.grey_light
+    color: Colors.grey_light,
   },
-  action_btns_container:{
+  action_btns_container: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    gap: 8
+    gap: 8,
   },
-  action_btn:{
+  action_btn: {
     paddingHorizontal: 12,
     paddingVertical: 6,
     borderColor: Colors.grey_light,
     backgroundColor: "white",
     borderRadius: 16,
-    borderWidth: 1
+    borderWidth: 1,
   },
 
-  action_text:{
+  action_text: {
     fontFamily: fonts.regular,
     fontSize: 12,
+    color: Colors.grey_normal,
+  },
+  coffees_container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 16,
+  },
+
+  coffee_detail_container: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  coffee_detail_description: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16
+  },
+
+  coffee_img_container:{
+    height: 54,
+    width: 54,
+    borderRadius: 8,
+    overflow: "hidden"
+  },
+  coffee_image:{
+    width: "100%",
+    height: "100%"
+  },
+  coffee_name:{
+    fontFamily: fonts.semibold,
+    fontSize: 16,
     color: Colors.grey_normal
+  },
+  coffee_tags:{
+    fontFamily: fonts.regular,
+    fontSize: 12,
+    color: Colors.grey_light
+  },
+  cart_action_btns:{
+     display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8
+  },
+  modify_coffee_count: {
+    height: 24,
+    width: 24,
+    borderRadius: "50%",
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  coffee_count: {
+    fontFamily: fonts.semibold,
+    color: Colors.grey_normal,
+    fontSize: 14
   }
 
 });
